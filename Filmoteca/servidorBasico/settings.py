c04fdc49c1e5926949e3984dcbc55170c057fabb
Alvaro Escudero <135117878+Alvaroescumed@users.noreply.github.com>
2024-07-20 13:45:50 +0200

    docker update

--- Filmoteca/servidorBasico/settings.py
+++ Filmoteca/servidorBasico/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*tinkl=(ae!)8t6fodb1vn-!k^j1&b&z(!!*2_3vd+@87f6%h7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
